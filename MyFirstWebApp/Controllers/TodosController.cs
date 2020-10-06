05a9d5eee395ed5f864f6cc044186a76d57b357a
Jean-Sébastien Castonguay <jscastonguay@gmail.com>
2020-10-06 17:15:07 -0400

    Ajout d'un todo fonctionne sans passer un objet Todo mais en passant chaque element séparé.

--- MyFirstWebApp/Controllers/TodosController.cs
+++ MyFirstWebApp/Controllers/TodosController.cs
@@ -62,7 +62,7 @@ namespace MyFirstWebApp.Controllers
         {
             TodosListModel current = new TodosListModel();
             current.counter = GetCounter();
-            
+
             ViewData["Counter"] = current.counter.counterValue;
 
             List<Todo> List = _context.Todo.ToList<Todo>();
@@ -76,6 +76,27 @@ namespace MyFirstWebApp.Controllers
             Counter current = GetCounter();
             current.counterValue++;
             _context.Counter.Update(current);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult AddTodo()
+        {
+            return View("AddTodo");
+        }
+
+        [HttpPost]
+        public IActionResult AddTodo(string description, int priority, int etiquette)
+        {
+            Console.WriteLine($"Voici la description: {description}");
+            
+            Todo newTodo = new Todo();
+            newTodo.description = description;
+            newTodo.priority = priority;
+            newTodo.etiquette = (Etiquette)etiquette;
+
+            _context.Todo.Add(newTodo);
             _context.SaveChanges();
 
             return RedirectToAction(nameof(Index));
